923d39484c94272f45c5e9aa14e254d628443c26
Adithyaranawaka77 <125335079+Adithyaranawaka77@users.noreply.github.com>
2024-03-20 00:42:39 +0530

    backedn start

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,11 @@ require("dotenv").config();
 //app
 const app = express();
 
+app.listen(5000,()=>console.log("server runing"))
+
 //db
 
-mongoose
+/*mongoose
 .connect(process.env.MONGO_URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -30,6 +32,6 @@ app.use("/",testRoutes);
 const port = process.env.PORT || 8080;
 
 //listener
-const server = app.listen(port, () =>
+ app.listen(port, () =>
 console.log(`server is running on port ${port}`)
-);
+);*/
